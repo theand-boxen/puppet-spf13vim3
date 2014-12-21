91a42ae2556f9cf918013a65f316d0a7c440016f
Heesang Chae <theand@gmail.com>
2014-12-21 16:03:51 +0900

    timeout 0

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 class spf13vim3 {
   exec {'install-spf13-vim-3':
     command => 'curl http://j.mp/spf13-vim3 -L | bash -s',
-    creates => "/Users/${::luser}/.spf13-vim-3"
+    creates => "/Users/${::luser}/.spf13-vim-3",
+    timeout => 0
   }
 }
